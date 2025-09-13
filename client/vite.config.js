9efb3d77d337c329630cc94965dfa922bbe000bc
Anmol <er.anmolghill@gmail.com>
2025-09-13 14:57:36 +0530

    update

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,14 +7,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-    open: true,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        secure: false
-      }
-    }
+    open: true
   },
   css: {
     postcss: {
